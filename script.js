217116c7cd062230a50e0814e48b33169e89e4d5
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-04-17 14:57:16 +0200

    inizializzata app vue e presi dati file json tramite api

---
+++ script.js
@@ -0,0 +1,23 @@
+const {createApp} = Vue
+
+createApp({
+  data(){
+    return{
+      apiUrl:'server.php',
+      listaDischi:[],
+
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(this.apiUrl)
+      .then(res=>{
+        this.listaDischi=res.data
+        console.log(this.listaDischi)
+      })
+    }
+  },
+  mounted(){
+    this.getApi()
+  }
+}).mount('#app')
